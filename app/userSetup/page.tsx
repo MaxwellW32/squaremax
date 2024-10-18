1c5793edf0c780eae1c56b7775edd5396acc9f85
quantumbuilder32 <quantumbuilder32@gmail.com>
2024-10-18 12:42:42 -0500

    working

--- app/userSetup/page.tsx
+++ app/userSetup/page.tsx
@@ -1,37 +1,37 @@
 "use client"
-import { updateNewUser } from '@/serverFunctions/handleUser';
-import { newUser } from '@/types';
-import { consoleErrorWithToast } from '@/usefulFunctions/consoleErrorWithToast';
-import React, { useState } from 'react'
-import { toast } from 'react-hot-toast';
+// import { updateNewUser } from '@/serverFunctions/handleUser';
+// import { newUser } from '@/types';
+// import { consoleErrorWithToast } from '@/usefulFunctions/consoleErrorWithToast';
+// import React, { useState } from 'react'
+// import { toast } from 'react-hot-toast';
 
 //go over all fields you want with a form
 //submit 
 //update user
 
 export default function Page() {
-    const initialFormObj: newUser = {}
-    const [formObj, formObjSet] = useState<newUser>({ ...initialFormObj })
+    // const initialFormObj: newUser = {}
+    // const [formObj, formObjSet] = useState<newUser>({ ...initialFormObj })
 
-    async function submitForm() {
-        try {
-            return
-            await updateNewUser(formObj)
+    // async function submitForm() {
+    //     try {
+    //         return
+    //         await updateNewUser(formObj)
 
-            toast.success("updated")
-        } catch (error) {
-            consoleErrorWithToast(error)
-        }
-    }
+    //         toast.success("updated")
+    //     } catch (error) {
+    //         consoleErrorWithToast(error)
+    //     }
+    // }
 
     return (
         <main>
 
-            <form action={() => { }}>
+            {/* <form action={() => { }}>
                 <button role='button'
                     onClick={submitForm}
                 >Complete Setup</button>
-            </form>
+            </form> */}
 
         </main>
     )
--- components/textArea/TextArea.tsx
+++ components/textArea/TextArea.tsx
@@ -1,5 +1,3 @@
-import styles from "./TextArea.module.css"
-
 export default function TextArea({ name, value, onInput, label, id, errors, errorColor = "#ff6c87", onBlur, placeHolder, isOptionalText, rows, ...elProps }: { name: string, value: string, onInput: (e: React.FormEvent<HTMLTextAreaElement>) => void, onBlur?: (e: React.FocusEvent<HTMLTextAreaElement, Element>) => void, label?: string, id?: string, errors?: string, errorColor?: string, placeHolder?: string, isOptionalText?: string, rows?: number } & React.HTMLAttributes<HTMLDivElement>) {
     return (
         <div {...elProps}>
