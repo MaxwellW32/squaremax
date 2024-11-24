e8f052c1cf3f68190caa71412317e0cccdc5a4e3
MaxwellW32 <maxwellwedderburn32@gmail.com>
2024-11-24 03:11:04 -0500

    multi file upload working

--- app/api/userImages/add/route.ts
+++ app/api/userImages/add/route.ts
@@ -7,27 +7,45 @@ import { ensureDirectoryExists } from "@/usefulFunctions/fileManagement";
 export async function POST(request: Request) {
     const formData = await request.formData();
     const body = Object.fromEntries(formData);
-    const file = (body.file as File) || null;
 
-    if (!file) {
-        throw new Error("didn't see file")
-    }
-
-    const id = uuidV4()
-    const imageType = file.type.split('/')[1]
-    const imageFileName = `${id}.${imageType}`
+    const bodyEntries = Object.entries(body)
     const imageDirectory = path.join(process.cwd(), "userUploadedData", "images")
-    const imagePath = path.join(imageDirectory, imageFileName)
 
     //ensure it exists
     await ensureDirectoryExists(imageDirectory)
 
-    const buffer = Buffer.from(await file.arrayBuffer());
+    const addedImageNames = await Promise.all(bodyEntries.map(async eachEntry => {
+        // const eachEntryKey = eachEntry[0] //file0, file1, file2
+        const eachEntryValueFile = eachEntry[1]
 
-    await fs.writeFile(imagePath, buffer);
+        const file = eachEntryValueFile as File;
+        const id = uuidV4()
+        const imageType = file.type.split('/')[1]
+        const imageFileName = `${id}.${imageType}`
+        const imagePath = path.join(imageDirectory, imageFileName)
+
+        const maxSizeInBytes = 5 * 1024 * 1024; // 5MB limit
+
+        // Check if file is an image (this will be redundant because of the 'accept' attribute, but can be good for double-checking)
+        if (!file.type.startsWith("image/")) {
+            throw new Error(`File is not an image.`)
+        }
+
+        // Check the file size
+        if (file.size > maxSizeInBytes) {
+            throw new Error(`File is too large. Maximum size is 5MB.`)
+        }
+
+        const buffer = Buffer.from(await file.arrayBuffer());
+
+        await fs.writeFile(imagePath, buffer);
+
+        return imageFileName
+    })
+    )
 
     return NextResponse.json({
-        name: imageFileName,
+        imageNames: addedImageNames,
     });
 }
 
--- components/projects/HandleUserUploadedImages.tsx
+++ components/projects/HandleUserUploadedImages.tsx
@@ -8,81 +8,105 @@ import { toast } from 'react-hot-toast'
 import { getSpecificProject, updateProject } from '@/serverFunctions/handleProjects'
 
 export default function HandleUserUploadedImages({ project, seenProjectSet }: { project: project, seenProjectSet: React.Dispatch<React.SetStateAction<project>> }) {
-    const [uploadedImage, uploadedImageSet] = useState<FormData | null>(null)
+    const [uploadedImages, uploadedImagesSet] = useState<FormData | null>(null)
     const imageStarterUrl = `https://squaremaxtech.com/api/userImages/view?imageName=`
 
     return (
         <div>
             <div style={{ display: "grid", alignContent: "flex-start", gap: "1rem" }}>
-                <input type="file" placeholder='Upload image'
+                <button className='mainButton' style={{ justifySelf: "flex-start" }}>
+                    <label htmlFor='fileUpload' style={{ cursor: "pointer" }}>
+                        upload images
+                    </label>
+                </button>
+
+                <input id='fileUpload' type="file" placeholder='Upload images' multiple={true} accept="image/*" style={{ display: "none" }}
                     onChange={(e) => {
                         if (!e.target.files) return
 
-                        const uploadedFile = e.target.files[0]
-
-                        console.log(`$uploadedFile`, uploadedFile);
-
+                        const maxSizeInBytes = 5 * 1024 * 1024; // 5MB limit
+                        const uploadedFiles = e.target.files
 
                         const formData = new FormData();
 
-                        formData.append("file", uploadedFile);
+                        for (let index = 0; index < uploadedFiles.length; index++) {
+                            const file = uploadedFiles[index];
 
-                        uploadedImageSet(formData)
+                            // Check if file is an image (this will be redundant because of the 'accept' attribute, but can be good for double-checking)
+                            if (!file.type.startsWith("image/")) {
+                                toast.error(`File ${file.name} is not an image.`);
+                                continue;
+                            }
+
+                            // Check the file size
+                            if (file.size > maxSizeInBytes) {
+                                toast.error(`File ${file.name} is too large. Maximum size is 5MB.`);
+                                continue;
+                            }
+
+                            formData.append(`file${index}`, uploadedFiles[index]);
+                        }
+
+                        uploadedImagesSet(formData)
                     }}
                 />
 
-                <button className='mainButton'
-                    onClick={async () => {
-                        try {
-                            if (uploadedImage === null) {
-                                throw new Error("need to upload an image")
+                {uploadedImages !== null && (
+                    <button className='mainButton'
+                        onClick={async () => {
+                            try {
+                                if (uploadedImages === null) {
+                                    throw new Error("need to upload an image")
+                                }
+
+                                const response = await fetch(`/api/userImages/add`, {
+                                    method: 'POST',
+                                    body: uploadedImages,
+                                })
+
+                                //array of image names
+                                const seenData = await response.json();
+
+                                //get the latest project images and upload project
+                                const latestProject = await getSpecificProject({ option: "id", data: { id: project.id } })
+                                if (latestProject === undefined) {
+                                    throw new Error("trouble updating, not seeing latest project")
+                                }
+
+                                let latestImagesSeen: project["userUploadedImages"] = latestProject.userUploadedImages
+
+                                if (latestImagesSeen === null) {
+                                    latestImagesSeen = [...seenData.imageNames]
+                                } else {
+                                    latestImagesSeen = [...latestImagesSeen, ...seenData.imageNames]
+                                }
+
+                                //update the server
+                                await updateProject({
+                                    id: project.id,
+                                    userUploadedImages: latestImagesSeen
+                                })
+
+                                //update local
+                                seenProjectSet(prevSeenProject => {
+                                    const newSeenProject = { ...prevSeenProject }
+
+                                    newSeenProject.userUploadedImages = latestImagesSeen
+
+                                    return newSeenProject
+                                })
+
+                                toast.success("images uploaded")
+
+                                uploadedImagesSet(null)
+
+                            } catch (error) {
+                                toast.error("Error adding image")
+                                console.error('Error adding image:', error);
                             }
-
-                            const response = await fetch(`/api/userImages/add`, {
-                                method: 'POST',
-                                body: uploadedImage,
-                            })
-
-                            const seenData = await response.json();
-                            //make multi upload
-
-                            //get the latest project images and upload project
-                            const latestProject = await getSpecificProject({ option: "id", data: { id: project.id } })
-                            if (latestProject === undefined) {
-                                throw new Error("trouble updating, not seeing latest project")
-                            }
-
-                            let latestImagesSeen: project["userUploadedImages"] = latestProject.userUploadedImages
-
-                            if (latestImagesSeen === null) {
-                                latestImagesSeen = [seenData.name]
-                            } else {
-                                latestImagesSeen = [...latestImagesSeen, seenData.name]
-                            }
-
-                            //update the server
-                            await updateProject({
-                                id: project.id,
-                                userUploadedImages: latestImagesSeen
-                            })
-
-                            //update local
-                            seenProjectSet(prevSeenProject => {
-                                const newSeenProject = { ...prevSeenProject }
-
-                                newSeenProject.userUploadedImages = latestImagesSeen
-
-                                return newSeenProject
-                            })
-
-                            toast.success("image uploaded")
-
-                        } catch (error) {
-                            toast.error("Error adding image")
-                            console.error('Error adding image:', error);
-                        }
-                    }}
-                >Add</button>
+                        }}
+                    >Add</button>
+                )}
             </div>
 
             {project.userUploadedImages !== null && (
