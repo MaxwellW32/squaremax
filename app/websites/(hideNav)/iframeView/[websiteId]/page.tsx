4844bbaf80fa146cc338edc6824042b3c0f61734
MaxwellW32 <maxwellwedderburn32@gmail.com>
2025-03-16 09:03:04 -0500

    yeah

--- app/websites/(hideNav)/iframeView/[websiteId]/page.tsx
+++ app/websites/(hideNav)/iframeView/[websiteId]/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { getSpecificWebsite } from '@/serverFunctions/handleWebsites'
 import { ensureUserCanAccessWebsite } from '@/usefulFunctions/sessionCheck'
-import ViewWebsite from '@/components/websites/RViewWebsite'
+import ViewWebsite from '@/components/websites/ViewWebsite'
 
 export default async function Page({ params }: { params: { websiteId: string } }) {
     const seenWebsite = await getSpecificWebsite({ option: "id", data: { id: params.websiteId } })
